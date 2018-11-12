442d9013a635d1b7d808918615bd35848aa924c4
Marah Rykard <rykardm@EMAIL.USCUPSTATE.EDU>
2018-11-12 15:16:54 -0500

    Added User Authorization to SurveyGiver/Index -Stefan

--- WebApplication4/Controllers/SurveyGiverController.cs
+++ WebApplication4/Controllers/SurveyGiverController.cs
@@ -14,12 +14,13 @@ namespace WebApplication4.Views
     {
         private SurveySaysDB2Entities db = new SurveySaysDB2Entities();
 
+
         // GET: SurveyGiver
         public ActionResult Index()
         {
             var user = System.Web.HttpContext.Current.User;
-            if (user.IsInRole("Taker")) {
-                return RedirectToRoute("SurveyTaker/Index");
+            if (!user.IsInRole("Giver")) {
+                return RedirectToRoute("SurveysTaker/Index");
             }
             var surveys = db.Surveys.Include(s => s.AspNetUser);
             return View(surveys.ToList());
