19b739c85b35fd9cc4accda3bf37c209514f01fd
Darrel Scott <descott@EMAIL.USCUPSTATE.EDU>
2018-11-20 22:42:37 -0500

    -made logo a little bigger -Account page now has all atributes centered, spaced out, and arranged -Added Basic stat area for future update on account page -Still need a way to display User email

--- WebApplication4/Models/ManageViewModels.cs
+++ WebApplication4/Models/ManageViewModels.cs
@@ -7,6 +7,7 @@ namespace WebApplication4.Models
 {
     public class IndexViewModel
     {
+        public bool HasEmail { get; set; }
         public bool HasPassword { get; set; }
         public IList<UserLoginInfo> Logins { get; set; }
         public string PhoneNumber { get; set; }
@@ -16,6 +17,7 @@ namespace WebApplication4.Models
 
     public class IndexGiverViewModel
     {
+        public bool HasEmail { get; set; }
         public bool HasPassword { get; set; }
         public IList<UserLoginInfo> Logins { get; set; }
         public string PhoneNumber { get; set; }
